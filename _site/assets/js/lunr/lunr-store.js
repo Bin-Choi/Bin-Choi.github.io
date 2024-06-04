4078838f91ac695f824b79a834928047040ab4a9
Bin-Choi <csb5268@gmail.com>
2024-06-04 17:49:13 +0900

    첫 게시물 포스팅

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "Blog Start",
+        "excerpt":"블로그 첫 게시물입니다~~~  블로그 시~~작 하겠습니닷!   ","categories": [],
+        "tags": [],
+        "url": "/my-first-post/",
+        "teaser": null
+      }]
